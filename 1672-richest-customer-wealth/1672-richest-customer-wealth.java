a5710b7062c5227ee43145492175f63380bdd978
Dave Wang <90726259+davewdh@users.noreply.github.com>
2022-08-14 10:47:37 -0400

    Time: 0 ms (100.00%), Space: 43 MB (75.26%) - LeetHub

---
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maximumWealth(int[][] accounts) {
+        int richest = 0;
+        for (int i = 0; i < accounts.length; i++) {
+            int wealth = 0;
+            for (int j = 0; j < accounts[0].length; j++) {
+                wealth += accounts[i][j];
+            }
+            if (wealth > richest)
+                richest = wealth;
+        }
+        return richest;
+    }
+}
