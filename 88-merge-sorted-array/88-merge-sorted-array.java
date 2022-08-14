9e73b050111f6358782f086f192e7b19d0532a57
Dave Wang <90726259+davewdh@users.noreply.github.com>
2022-08-14 16:56:29 -0400

    Time: 0 ms (100.00%), Space: 43.4 MB (9.82%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.java
+++ 88-merge-sorted-array/88-merge-sorted-array.java
@@ -1,12 +1,21 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int index = 0;
-        for (int i = m; i < m+n; i++) {
-            nums1[i] = nums2[index];
-            index++;
+        int index1 = m - 1, index2 = n - 1, index3 = m + n - 1;
+        while (index1 >= 0 && index2 >= 0) {
+            if (nums1[index1] < nums2[index2]) {
+                nums1[index3] = nums2[index2];
+                index3--;
+                index2--;
+            } else {
+                nums1[index3] = nums1[index1];
+                index3--;
+                index1--;
+            }
         }
-        Arrays.sort(nums1);
-        
-        
+        while (index2 >= 0) {
+            nums1[index3] = nums2[index3];
+            index3--;
+            index2--;
+        }
     }
 }
