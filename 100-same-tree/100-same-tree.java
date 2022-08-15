5dd56d226af4bbdac725988452c765f4b5fe3bf2
Dave Wang <90726259+davewdh@users.noreply.github.com>
2022-08-15 17:53:32 -0400

    Time: 0 ms (100.00%), Space: 41.9 MB (21.53%) - LeetHub

--- 100-same-tree/100-same-tree.java
+++ 100-same-tree/100-same-tree.java
@@ -15,23 +15,13 @@
  */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        if ((p == null)) {
-            if ((q == null)) 
-                return true;
-            else
-                return false;
-        }
-        else {
-            if (q == null)
-                return false;
-            else {
-                if (p.val != q.val)
-                    return false;
-                else
-                    return (isSameTree(p.left, q.left) && isSameTree(p.right, q.right));
-            }
-                
-        }
+        if ((p == null) && (q == null))
+            return true;
+        if ((p == null) || (q == null))
+            return false;
+        if (p.val != q.val)
+            return false;
+        return (isSameTree(p.left, q.left) && isSameTree(p.right, q.right));  
         
     }
 }
