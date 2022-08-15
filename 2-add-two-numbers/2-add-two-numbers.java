b2b3ba0e46fd66c31a9c4e9c4c9dd8026ba62fce
Dave Wang <90726259+davewdh@users.noreply.github.com>
2022-08-15 19:05:57 -0400

    Time: 3 ms (80.48%), Space: 47.7 MB (64.09%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.java
+++ 2-add-two-numbers/2-add-two-numbers.java
@@ -10,7 +10,7 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode head = new ListNode(1);
+        ListNode head = new ListNode();
         ListNode current = head;
         int carry = 0;
         while (l1 != null || l2 != null) {
@@ -26,10 +26,6 @@ class Solution {
             current.next = new ListNode(1);
         
         return head.next;
-        
-        
-        
-        
         
         
         // if (l1 == null && l2 == null)
