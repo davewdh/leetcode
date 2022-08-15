1640dc471692802181de6c079c112264b4756b25
Dave Wang <90726259+davewdh@users.noreply.github.com>
2022-08-15 12:35:57 -0400

    Time: 0 ms (100.00%), Space: 40.2 MB (97.26%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.java
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.java
@@ -15,20 +15,7 @@
  */
 class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
-        // Stack<TreeNode> stack = new Stack<>();
-        // List<Integer> aList = new ArrayList<>();
-        // if (root == null)
-        //     return aList;
-        // stack.push(root);
-        // while (stack.size() > 0) {
-        //     TreeNode current = stack.pop();
-        //     aList.add(current.val);
-        //     if (current.right != null)
-        //         stack.push(current.right);
-        //     if (current.left != null)
-        //         stack.push(current.left);
-        // }
-        // return aList;
+        
         
         List<Integer> arr = new ArrayList<>();
         if (root == null) 
