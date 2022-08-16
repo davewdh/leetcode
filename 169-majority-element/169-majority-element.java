541f83b97d4e3dbcb940849cb74f7ae4db1b1044
Dave Wang <90726259+davewdh@users.noreply.github.com>
2022-08-16 17:32:27 -0400

    Time: 28 ms (8.19%), Space: 54 MB (85.14%) - LeetHub

--- 169-majority-element/169-majority-element.java
+++ 169-majority-element/169-majority-element.java
@@ -6,6 +6,8 @@ class Solution {
         for (int i = 0; i < nums.length; i++) {
             if (map.containsKey(nums[i])) {
                 map.put(nums[i], map.get(nums[i]) + 1);
+                if (map.get(nums[i]) > nums.length/2)
+                    return nums[i];
                 if (map.get(nums[i]) > max) {
                     max = map.get(nums[i]);
                     majority = nums[i];
