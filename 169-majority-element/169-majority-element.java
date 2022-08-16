a6bb98fcc2d660aa85ce766cc9b07cead04c52df
Dave Wang <90726259+davewdh@users.noreply.github.com>
2022-08-16 17:38:34 -0400

    Time: 5 ms (46.60%), Space: 56.5 MB (35.58%) - LeetHub

--- 169-majority-element/169-majority-element.java
+++ 169-majority-element/169-majority-element.java
@@ -1,22 +1,24 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        Map<Integer, Integer> map = new HashMap<>();
-        int majority = nums[0];
-        int max = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (map.containsKey(nums[i])) {
-                map.put(nums[i], map.get(nums[i]) + 1);
-                if (map.get(nums[i]) > max) {
-                    max = map.get(nums[i]);
-                    majority = nums[i];
-                    if (max > nums.length/2)
-                        return majority;
-                    
-                }
-            } 
-            else
-                map.put(nums[i], 1);
-        }
-        return majority;
+        Arrays.sort(nums);
+        return nums[nums.length/2];
+        
+        // Map<Integer, Integer> map = new HashMap<>();
+        // int majority = nums[0];
+        // int max = 0;
+        // for (int i = 0; i < nums.length; i++) {
+        //     if (map.containsKey(nums[i])) {
+        //         map.put(nums[i], map.get(nums[i]) + 1);
+        //         if (map.get(nums[i]) > max) {
+        //             max = map.get(nums[i]);
+        //             majority = nums[i];
+        //             if (max > nums.length/2)
+        //                 return majority; 
+        //         }
+        //     } 
+        //     else
+        //         map.put(nums[i], 1);
+        // }
+        // return majority;
     }
 }
