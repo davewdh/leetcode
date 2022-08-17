576e52d2dab8d9868fa117355a955edafcb3d8eb
Dave Wang <90726259+davewdh@users.noreply.github.com>
2022-08-17 13:00:30 -0400

    Time: 1 ms (99.61%), Space: 40.2 MB (96.42%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.java
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -1,18 +1,45 @@
 class Solution {
     public boolean isValid(String s) {
         Stack<Character> aStack = new Stack<>();
+        char c;
         for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            if (c == '(' || c == '{' || c == '[')
-                aStack.push(c);
-            if (c == ')' || c == '}' || c == ']') {
-                if (aStack.isEmpty())
-                    return false;
-                char top = aStack.pop();
-                if ((top != '(') && (c == ')') || ((top != '{') && (c == '}')) || ((top != '[') && (c == ']')))
-                    return false;
+            c = s.charAt(i);
+            switch(c) {
+                case '(':
+                case '{':
+                case '[':
+                    aStack.push(c);
+                    break;
+                case ')':
+                    if ((aStack.isEmpty()) || (aStack.pop() != '(')) 
+                        return false;
+                    break;
+                case '}':
+                    if ((aStack.isEmpty()) || (aStack.pop() != '{')) 
+                        return false;
+                    break;
+                case ']':
+                    if ((aStack.isEmpty()) || (aStack.pop() != '[')) 
+                        return false;
+                    break;
             }
         }
         return aStack.isEmpty();
+        
+        // Stack<Character> aStack = new Stack<>();
+        // char c;
+        // for (int i = 0; i < s.length(); i++) {
+        //     c = s.charAt(i);
+        //     if (c == '(' || c == '{' || c == '[')
+        //         aStack.push(c);
+        //     if (c == ')' || c == '}' || c == ']') {
+        //         if (aStack.isEmpty())
+        //             return false;
+        //         char top = aStack.pop();
+        //         if ((top != '(') && (c == ')') || ((top != '{') && (c == '}')) || ((top != '[') && (c == ']')))
+        //             return false;
+        //     }
+        // }
+        // return aStack.isEmpty();
     }
 }
