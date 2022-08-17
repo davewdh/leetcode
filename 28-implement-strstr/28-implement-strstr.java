46e97f8dd1412d41abfbbea4e1306183a5669367
Dave Wang <90726259+davewdh@users.noreply.github.com>
2022-08-17 17:34:21 -0400

    Time: 0 ms (100.00%), Space: 42.4 MB (15.85%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.java
+++ 28-implement-strstr/28-implement-strstr.java
@@ -1,7 +1,28 @@
 class Solution {
     public int strStr(String haystack, String needle) {
-        if (needle == "")
+        if ((needle == "") ||(needle.equals(haystack)))
             return 0;
-        return haystack.indexOf(needle);
+        for (int i = 0; i < haystack.length(); i++) {
+            int count = 0;
+            if ((haystack.charAt(i) == needle.charAt(0)) && (needle.length() <= haystack.length()-i)) {
+                if (needle.length() == 1)
+                    return i;
+                for (int j = 1; j < needle.length(); j++) {
+                    if (haystack.charAt(i+j) != needle.charAt(j))
+                        break;
+                    count++;
+                    if (count == needle.length()-1)
+                        return i;
+                }
+            }
+        }
+        return -1;
+        
+        
+        
+        
+        
+        
+        // return haystack.indexOf(needle);
     }
 }
