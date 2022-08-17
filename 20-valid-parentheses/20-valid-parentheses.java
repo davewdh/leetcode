4b01981966a85c4ced2704c1324dbd721c63e7cf
Dave Wang <90726259+davewdh@users.noreply.github.com>
2022-08-17 12:47:04 -0400

    Time: 3 ms (62.32%), Space: 42.2 MB (43.70%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.java
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -2,33 +2,16 @@ class Solution {
     public boolean isValid(String s) {
         Stack<Character> aStack = new Stack<>();
         for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == '(' || s.charAt(i) == '{' || s.charAt(i) == '[')
-                aStack.push(s.charAt(i));
-            if (s.charAt(i) == ')' || s.charAt(i) == '}' || s.charAt(i) == ']') {
+            char c = s.charAt(i);
+            if (c == '(' || c == '{' || c == '[')
+                aStack.push(c);
+            if (c == ')' || c == '}' || c == ']') {
                 if (aStack.isEmpty())
                     return false;
-            
-                
-                switch (s.charAt(i)) {
-                    case ')' :
-                        if (aStack.pop() != '(')
-                            return false;
-                        break;
-                    case '}' :
-                        if (aStack.pop() != '{')
-                            return false;
-                        break;
-                    case ']' :
-                        if (aStack.pop() != '[')
-                            return false;
-                        break;
-                    default:
-                            return false;
-                }
+                char top = aStack.pop();
+                if ((top != '(') && (c == ')') || ((top != '{') && (c == '}')) || ((top != '[') && (c == ']')))
+                    return false;
             }
-                
-                
-        
         }
         return aStack.isEmpty();
     }
