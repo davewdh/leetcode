4b1a5e59a34c2534be02f6349638c2ddf478e492
Dave Wang <90726259+davewdh@users.noreply.github.com>
2022-08-17 19:36:30 -0400

    Time: 292 ms (5.01%), Space: 41.5 MB (37.18%) - LeetHub

--- 69-sqrtx/69-sqrtx.java
+++ 69-sqrtx/69-sqrtx.java
@@ -5,7 +5,7 @@ class Solution {
         if (x == 1)
             return 1;
         
-        for (int i = 1; i <= x/i; i++) {
+        for (int i = 1; i <= x/2; i++) {
             if ((i <= x/i) && ( i+1 > (x / (i+1))))
                 return i;  
         }
