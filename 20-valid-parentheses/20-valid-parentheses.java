595d2d714813c4bd751559c122b7b3dfdf3ce793
Dave Wang <90726259+davewdh@users.noreply.github.com>
2022-08-17 13:16:24 -0400

    Time: 1 ms (99.61%), Space: 40.1 MB (96.42%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.java
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -4,42 +4,45 @@ class Solution {
         char c;
         for (int i = 0; i < s.length(); i++) {
             c = s.charAt(i);
-            switch(c) {
-                case '(':
-                case '{':
-                case '[':
-                    aStack.push(c);
-                    break;
-                case ')':
-                    if ((aStack.isEmpty()) || (aStack.pop() != '(')) 
-                        return false;
-                    break;
-                case '}':
-                    if ((aStack.isEmpty()) || (aStack.pop() != '{')) 
-                        return false;
-                    break;
-                case ']':
-                    if ((aStack.isEmpty()) || (aStack.pop() != '[')) 
-                        return false;
-                    break;
-            }
+            if (c == '(')
+                aStack.push(')');
+            else if (c == '{')
+                aStack.push('}');
+            else if (c == '[')
+                aStack.push(']');
+            else if ((aStack.isEmpty()) || (aStack.pop() != c)) 
+                return false;  
         }
         return aStack.isEmpty();
+        
+        
         
         // Stack<Character> aStack = new Stack<>();
         // char c;
         // for (int i = 0; i < s.length(); i++) {
         //     c = s.charAt(i);
-        //     if (c == '(' || c == '{' || c == '[')
-        //         aStack.push(c);
-        //     if (c == ')' || c == '}' || c == ']') {
-        //         if (aStack.isEmpty())
-        //             return false;
-        //         char top = aStack.pop();
-        //         if ((top != '(') && (c == ')') || ((top != '{') && (c == '}')) || ((top != '[') && (c == ']')))
-        //             return false;
+        //     switch(c) {
+        //         case '(':
+        //         case '{':
+        //         case '[':
+        //             aStack.push(c);
+        //             break;
+        //         case ')':
+        //             if ((aStack.isEmpty()) || (aStack.pop() != '(')) 
+        //                 return false;
+        //             break;
+        //         case '}':
+        //             if ((aStack.isEmpty()) || (aStack.pop() != '{')) 
+        //                 return false;
+        //             break;
+        //         case ']':
+        //             if ((aStack.isEmpty()) || (aStack.pop() != '[')) 
+        //                 return false;
+        //             break;
         //     }
         // }
         // return aStack.isEmpty();
+        
+        
     }
 }
