20a6b1230ffce8e68ce02b5ba417f07ac39c7123
Dave Wang <90726259+davewdh@users.noreply.github.com>
2022-08-18 17:17:49 -0400

    Time: 0 ms (100.00%), Space: 41 MB (43.31%) - LeetHub

--- 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.java
+++ 1486-xor-operation-in-an-array/1486-xor-operation-in-an-array.java
@@ -1,9 +1,8 @@
 class Solution {
     public int xorOperation(int n, int start) {
         int num = start + 2 * 0;
-        for (int i = 1; i < n; i++) {
+        for (int i = 1; i < n; i++) 
             num = num ^ (start + 2 * i);
-        }
         return num;
     }
 }
