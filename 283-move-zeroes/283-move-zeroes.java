53b022efac21713a45dcb9a348aff99ace18ec30
Daohu(Dave) Wang <90726259+davewdh@users.noreply.github.com>
2022-08-24 10:10:55 -0400

    Time: 5 ms (15.28%), Space: 55.1 MB (29.08%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.java
+++ 283-move-zeroes/283-move-zeroes.java
@@ -1,12 +1,9 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int i = 0;
-        int j = 1;
-        for(i=0,j=1;j<nums.length;i++,j++){
+        for(int i=0,j=1;j<nums.length;i++,j++){
             if (nums[i] == 0 & nums[j] != 0) {
-                int temp = nums[i];
                 nums[i] = nums[j];
-                nums[j] = temp;
+                nums[j] = 0;
             }
             if (nums[i] == 0 & nums[j] == 0) {
                 i--;
