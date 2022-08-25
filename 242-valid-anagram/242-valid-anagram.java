389bfee2d801783618a562fc138890142d31c3c9
Daohu(Dave) Wang <90726259+davewdh@users.noreply.github.com>
2022-08-25 19:30:06 -0400

    Time: 8 ms (42.06%), Space: 43.6 MB (63.25%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.java
+++ 242-valid-anagram/242-valid-anagram.java
@@ -2,10 +2,17 @@ class Solution {
     public boolean isAnagram(String s, String t) {
         if (s.length() != t.length())
             return false;
-        char[] a = s.toCharArray();
-        Arrays.sort(a);
-        char[] b = t.toCharArray();
-        Arrays.sort(b);
-        return Arrays.equals(a, b);
+        int[] arr = new int[26];
+        
+        for (int i = 0; i < s.length(); i++) {
+            arr[s.charAt(i)-'a']++;
+            arr[t.charAt(i)-'a']--;
+        }
+        
+        for (int i : arr) {
+            if (i != 0)
+                return false;
+        }
+        return true;
     }
 }
