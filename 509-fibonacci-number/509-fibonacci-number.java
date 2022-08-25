feaf31cb1e5b8a5460b0dbcc6e572fed28c1a379
Daohu(Dave) Wang <90726259+davewdh@users.noreply.github.com>
2022-08-25 18:43:55 -0400

    Time: 9 ms (33.98%), Space: 41.1 MB (26.31%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -1,17 +1,20 @@
 class Solution {
     public int fib(int n) {
-        if (n <= 1)
+        if ( n <= 1)
             return n;
-        int a = 0;
-        int b = 1;
-        int sum = 0;
-        int i = 2;
-        while (i <= n) {
-            sum = a + b;
-            a = b;
-            b = sum;
-            i++;
-        }
-        return sum;
+        return fib(n-1) + fib(n-2);
+        // if (n <= 1)
+        //     return n;
+        // int a = 0;
+        // int b = 1;
+        // int sum = 0;
+        // int i = 2;
+        // while (i <= n) {
+        //     sum = a + b;
+        //     a = b;
+        //     b = sum;
+        //     i++;
+        // }
+        // return sum;
     }
 }
