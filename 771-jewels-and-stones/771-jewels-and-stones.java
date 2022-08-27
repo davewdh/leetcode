6594f7ebcf4dad39993b2b89b8caf096744d5763
Daohu(Dave) Wang <90726259+davewdh@users.noreply.github.com>
2022-08-27 10:07:32 -0400

    Time: 1 ms (98.04%), Space: 42.6 MB (27.29%) - LeetHub

--- 771-jewels-and-stones/771-jewels-and-stones.java
+++ 771-jewels-and-stones/771-jewels-and-stones.java
@@ -2,8 +2,7 @@ class Solution {
     public int numJewelsInStones(String jewels, String stones) {
         int count = 0;
         for (int i = 0; i < stones.length(); i++) {
-            
-            if (jewels.contains(stones.charAt(i) + ""))
+            if (jewels.indexOf(stones.charAt(i)) != -1)
                 count++;
         }
         return count;
