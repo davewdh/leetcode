8c857e3d3f11825f85d93e050a23ceaead556c6d
Daohu(Dave) Wang <90726259+davewdh@users.noreply.github.com>
2022-08-28 09:16:45 -0400

    Time: 0 ms (100.00%), Space: 42.2 MB (5.36%) - LeetHub

---
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int numberOfMatches(int n) {
+        int count = 0;
+        while (n != 1) {
+            if (n % 2 == 0) {
+                n = n / 2;
+                count += n;
+            } else {
+                n = (n-1) / 2;
+                count += n + 1;
+            }
+        }
+        return count;
+    }
+}
