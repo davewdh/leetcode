a179504670be157c6e3a41092ce3193780be50f7
Daohu Wang <90726259+davewdh@users.noreply.github.com>
2025-06-29 22:12:48 -0400

    Time: 11 ms (26.73%), Space: 45.5 MB (5.95%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -1,23 +1,23 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        HashMap<Integer, HashSet<Character>> rows = new HashMap<>();
-        HashMap<Integer, HashSet<Character>> cols = new HashMap<>();
-        HashMap<String, HashSet<Character>> squares = new HashMap<>();
-        
-        for (int r = 0; r < 9; r++){
+        HashMap<Integer, HashSet<Integer>> rows = new HashMap<>();
+        HashMap<Integer, HashSet<Integer>> cols = new HashMap<>();
+        HashMap<String, HashSet<Integer>> squares = new HashMap<>();
+        for (int r = 0; r < 9; r++) {
             for (int c = 0; c < 9; c++) {
-                char val = board[r][c];
+                int val = board[r][c];
                 if (val == '.') {
                     continue;
                 }
-                String squareKey = (r / 3) + "," + (c / 3);
+                String squareKey = (r/3) + "," + (c/3);
                 rows.putIfAbsent(r, new HashSet<>());
                 cols.putIfAbsent(c, new HashSet<>());
                 squares.putIfAbsent(squareKey, new HashSet<>());
 
                 if (rows.get(r).contains(val) || cols.get(c).contains(val) || squares.get(squareKey).contains(val)) {
                     return false;
-                } 
+                }
+
                 rows.get(r).add(val);
                 cols.get(c).add(val);
                 squares.get(squareKey).add(val);
