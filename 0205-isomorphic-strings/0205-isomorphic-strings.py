a1bdf1fb6502bab222a7c41702a246d75b132752
Daohu Wang <90726259+davewdh@users.noreply.github.com>
2025-07-26 22:28:21 -0400

    Time: 11 ms (15.32%), Space: 18.1 MB (17.27%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -1,16 +1,11 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
-        if len(s) != len(t):
-            return False
-        if s == t:
+        def helper(s, t):
+            dic = {}
+            for i in range(len(s)):
+                if s[i] in dic:
+                    if dic[s[i]] != t[i]:
+                        return False
+                dic[s[i]] = t[i]
             return True
-        if len("".join(set(s))) != len("".join(set(t))):
-            return False
-        dic = {}
-        for i in range(len(s)):
-            if s[i] in dic:
-                if dic[s[i]] != t[i]:
-                    return False
-            dic[s[i]] = t[i]
-        return True
-        
+        return helper(s, t) and helper(t, s)
