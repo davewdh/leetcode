9ff82d1c066d037ffbe64321ee022b9dd2c0cf13
Daohu Wang <90726259+davewdh@users.noreply.github.com>
2025-08-19 21:56:24 -0400

    Time: 0 ms (100%), Space: 17.6 MB (95.51%) - LeetHub

--- 1547-destination-city/1547-destination-city.py
+++ 1547-destination-city/1547-destination-city.py
@@ -1,11 +1,9 @@
 class Solution:
     def destCity(self, paths: List[List[str]]) -> str:
-        m = {}
+        s= set()
         for p in paths:
-            m[p[0]] = p[1]
-            if p[1] not in m:
-                m[p[1]] = ""
-        for k, v in m.items():
-            if v == "":
-                return k                
+            s.add(p[0])   
+        for p in paths:
+            if p[1] not in s:
+                return p[1]
                 
