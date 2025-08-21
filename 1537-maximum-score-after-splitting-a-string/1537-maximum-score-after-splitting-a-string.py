0096c17c70183d3d2068dff99e2a6f550514762f
Daohu Wang <90726259+davewdh@users.noreply.github.com>
2025-08-20 21:48:43 -0400

    Time: 0 ms (100%), Space: 17.9 MB (20.57%) - LeetHub

--- 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.py
+++ 1537-maximum-score-after-splitting-a-string/1537-maximum-score-after-splitting-a-string.py
@@ -3,12 +3,7 @@ class Solution:
         ans = 0
         ones = s.count("1")
         zeros = 0
-        if s[0] == "0":
-            zeros = 1
-        else:
-            ones -= 1
-        ans = max(ans, zeros + ones)
-        for i in range(1, len(s)-1):
+        for i in range(len(s)-1):
             if s[i] == "0":
                 zeros += 1
             else:
