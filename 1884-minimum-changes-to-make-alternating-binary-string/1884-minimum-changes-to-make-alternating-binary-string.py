70a3e6afa5d03282f98b75ab08690c1f96cf64d4
Daohu Wang <90726259+davewdh@users.noreply.github.com>
2025-08-21 21:43:58 -0400

    Time: 7 ms (86.28%), Space: 17.8 MB (62.37%) - LeetHub

--- 1884-minimum-changes-to-make-alternating-binary-string/1884-minimum-changes-to-make-alternating-binary-string.py
+++ 1884-minimum-changes-to-make-alternating-binary-string/1884-minimum-changes-to-make-alternating-binary-string.py
@@ -3,17 +3,15 @@ class Solution:
         ans1 = 0
         ans2 = 0
         for i in range(len(s)):
-            # 101010101...
-            if i % 2 != 0:
-                if s[i] == "0":
+            if s[i] == "0":
+                if i % 2 != 0:
                     ans1 += 1
-                if s[i] == "1":
+                else:
                     ans2 += 1
             else:
-                if s[i] == "1":
-                    ans1 += 1
-                if s[i] == "0":
+                if i % 2 != 0:
                     ans2 += 1
-
+                else:
+                    ans1 += 1
         return min(ans1, ans2)
         
