6297f927b0a0afe7840f556aebc6d80bf89c12fe
Daohu Wang <90726259+davewdh@users.noreply.github.com>
2025-08-30 22:14:37 -0400

    Time: 0 ms (100%), Space: 17.8 MB (90.8%) - LeetHub

--- 1217-relative-sort-array/1217-relative-sort-array.py
+++ 1217-relative-sort-array/1217-relative-sort-array.py
@@ -9,12 +9,8 @@ class Solution:
                 m[n] = 1
         
         for n1 in arr2:
-            if m.get(n1):
-                for i in range(m.get(n1)):
-                    ans.append(n1)
-            m.pop(n1)
+            ans += [n1] * m.pop(n1)
         
-        for k, v in sorted(m.items()):
-            for j in range(v):
-                ans.append(k)
+        for k in sorted(m):
+            ans += [k] * m[k]
         return ans
