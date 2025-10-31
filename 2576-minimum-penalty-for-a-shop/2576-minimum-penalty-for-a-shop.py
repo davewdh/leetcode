d5a55de5712743cbe9b6e15e38584e3181863a65
Daohu Wang <90726259+davewdh@users.noreply.github.com>
2025-10-30 22:33:25 -0400

    Time: 130 ms (41.85%), Space: 22.6 MB (35.91%) - LeetHub

--- 2576-minimum-penalty-for-a-shop/2576-minimum-penalty-for-a-shop.py
+++ 2576-minimum-penalty-for-a-shop/2576-minimum-penalty-for-a-shop.py
@@ -1,13 +1,13 @@
 class Solution:
     def bestClosingTime(self, customers: str) -> int:
         n = len(customers)
-        prefix_n, postfix_y = [0], [0] * (n + 1)
+        prefix_n, postfix_y = [0] * (n + 1), [0] * (n + 1)
         ans, count = 0, 0
 
-        for i in range(n):
-            if customers[i] == "N":
+        for i in range(1, n+1):
+            if customers[i-1] == "N":
                 count += 1
-            prefix_n.append(count)
+            prefix_n[i] = count
 
         count = 0
         for i in range(n-1, -1, -1):
