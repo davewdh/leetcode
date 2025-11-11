471ae059f2791c46ccb97d82f4271e3c87df314f
Daohu Wang <90726259+davewdh@users.noreply.github.com>
2025-11-10 22:13:22 -0500

    Time: 21 ms (98.48%), Space: 19.2 MB (70.41%) - LeetHub

--- 1371-minimum-remove-to-make-valid-parentheses/1371-minimum-remove-to-make-valid-parentheses.py
+++ 1371-minimum-remove-to-make-valid-parentheses/1371-minimum-remove-to-make-valid-parentheses.py
@@ -4,22 +4,15 @@ class Solution:
         temp = list(s)
         for i in range(len(s)):
             if s[i] == "(":
-                stack.append(s[i])
+                stack.append(i)
             elif s[i] == ")":
                 if stack:
                     stack.pop()
                 else:
                     temp[i] = ""
 
-        temp1 = temp
-        s1 = []
-        for i in range(len(temp1)-1, -1 , -1):
-            if temp1[i] == ")":
-                s1.append(temp[i])
-            elif temp1[i] == "(":
-                if s1:
-                    s1.pop()
-                else:
-                    temp1[i] = ""
-        return "".join(temp1)
+        while stack:
+            temp[stack.pop()] = ""
+
+        return "".join(temp)
             
