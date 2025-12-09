1f27ba17e071eede2f307dd79ea4d7fa2f92f88b
Daohu Wang <90726259+davewdh@users.noreply.github.com>
2025-12-08 22:13:18 -0500

    Time: 0 ms (100%), Space: 17.9 MB (32.86%) - LeetHub

---
+++ 1813-sentence-similarity-iii/1813-sentence-similarity-iii.py
@@ -0,0 +1,28 @@
+class Solution:
+    def areSentencesSimilar(self, sentence1: str, sentence2: str) -> bool:
+        s1 = sentence1.split(" ")
+        s2 = sentence2.split(" ")
+        if len(s1) > len(s2):
+            s1, s2 = s2, s1
+        l, r1, r2 = 0, len(s1) -1, len(s2)-1
+        prefix, suffix = "", ""
+        while l <= r1 and s1[l] == s2[l]:
+            prefix += s1[l]
+            l += 1
+        while l <= r1 and s1[r1] == s2[r2]:
+            print(1)
+            temp = s1[r1] + suffix
+            print(temp)
+            suffix  = temp
+            print(suffix)
+            r1 -= 1
+            r2 -= 1
+            
+     
+        print(prefix + suffix)
+        print("".join(s1))
+        if (prefix + suffix) == "".join(s1):
+            return True
+        else:
+            return False
+      
