db4ac545b1932c13b65e714626103570da529693
Daohu Wang <90726259+davewdh@users.noreply.github.com>
2025-12-14 21:59:51 -0500

    Time: 22 ms (32.87%), Space: 19.4 MB (45.87%) - LeetHub

--- 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.py
+++ 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.py
@@ -2,25 +2,20 @@ class Solution:
     def maxSatisfied(self, customers: List[int], grumpy: List[int], minutes: int) -> int:
         sat = 0
         not_sat = 0
-        for i in range(minutes):
-            if grumpy[i] == 0:
-                sat += customers[i]
-            else:
-                not_sat += customers[i]
-        max_not_sat = not_sat
-        l = 0
-        r = minutes
+        max_not_sat = 0
+        l, r = 0, 0
         while r < len(customers):
             if grumpy[r] == 0:
                 sat += customers[r]
             else:
                 not_sat += customers[r]
-            if grumpy[l] == 1:
-                not_sat -= customers[l] 
+            if r - l + 1 > minutes:
+                if grumpy[l] == 1:
+                    not_sat -= customers[l] 
+                l += 1
             max_not_sat = max(max_not_sat, not_sat)
             r += 1
-            l += 1
-
+            
         return sat + max_not_sat
 
         
