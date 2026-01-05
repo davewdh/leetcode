0958d26c796c9a4f57c59c5173a53dbdf2256f8a
Daohu Wang <90726259+davewdh@users.noreply.github.com>
2026-01-04 21:00:17 -0500

    Time: 15 ms (28.77%), Space: 17.2 MB (95.89%) - LeetHub

---
+++ 1190-reverse-substrings-between-each-pair-of-parentheses/1190-reverse-substrings-between-each-pair-of-parentheses.py
@@ -0,0 +1,16 @@
+class Solution:
+    def reverseParentheses(self, s: str) -> str:
+        stack = []
+
+        for c in s:
+            if c == ")":
+                temp = []
+                while stack and stack[-1] != "(":
+                    temp.append(stack.pop())
+                stack.pop()
+                stack.extend(temp)
+            else:
+                stack.append(c)
+        
+        return "".join(stack)
+                
